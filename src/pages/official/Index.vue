10728c3f21627e0b1e5627800c43a2200231e714
huzheng <zheng.hu@myt11.com>
2020-02-01 13:05:35 +0800

    填充官网主要尾部

--- src/pages/official/Index.vue
+++ src/pages/official/Index.vue
@@ -5,12 +5,19 @@
         <introduction/>
         <culture/>
         <honor/>
+        <div class="set-div-size">
+            <div class="honor-index-div1">
+                Git in touch
+            </div>
+            <div class="honor-index-div2">
+                Serch in index
+            </div>
+        </div>
         <div align='center' style="margin-top:15px">
             Copyright©2018企业产品科技集团股份有限公司<br>
             版权所有京ICP证140608号.京ICP备12041691号-2.京公网安备11010502026582<br>
             互联网药品信息服务资格证书 （京）-非经营性-2017-0039<br>
         </div>
-        <el-backtop></el-backtop>
     </div>
 </template>
 
@@ -41,5 +48,16 @@ export default {
 </script>
 
 <style>
- 
+ .set-div-size{
+     width: 100%;
+     height: 400px;
+ }
+ .honor-index-div1{
+     float: left;
+     width: 50%;
+ }
+.honor-index-div2{
+     float: right;
+     width: 50%;
+ }
 </style>
