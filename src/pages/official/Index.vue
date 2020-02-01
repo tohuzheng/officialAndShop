ef3de7b1254133c3efe9db2260920fd4be95b333
huzheng <zheng.hu@myt11.com>
2020-02-01 18:11:12 +0800

    企业官网部分布局完成，导航点击事件未实现

--- src/pages/official/Index.vue
+++ src/pages/official/Index.vue
@@ -5,12 +5,66 @@
         <introduction/>
         <culture/>
         <honor/>
-        <div class="set-div-size">
+        <div class="tail-div-size">
             <div class="honor-index-div1">
-                Git in touch
+                <div class="left-tail-top-div">
+                    <div style="margin-bottom : 30px;">Get in touch</div>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </div>
+                <div>
+                    <div class="left-tail-div-size">
+                        <div style="margin-bottom:30px;">
+                            <div>Resources</div>
+                        </div>
+                        <div>
+                            Tour<br>
+                            Customers<br>
+                            Pricing and Plans<br>
+                            New Features<br>
+                            Education
+                        </div>
+                    </div>
+                    <div class="left-tail-div-size">
+                        <div style="margin-bottom : 30px;">Features</div>
+                        <div>
+                            Tour<br>
+                            Customers<br>
+                            Pricing and Plans<br>
+                            New Features<br>
+                            Education
+                        </div>
+                    </div>
+                    <div class="left-tail-div-size">
+                        <div style="margin-bottom : 30px;">How To's</div>
+                        <div>
+                            Tour<br>
+                            Customers<br>
+                            Pricing and Plans<br>
+                            New Features<br>
+                            Education
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="honor-index-div2">
-                Serch in index
+                <div>
+                    <div style="margin:40px 40px;">Stay in touch</div>
+                </div>
+                <div style="margin:20px 40px;">
+                    <el-input
+                    placeholder="请输入内容"
+                    v-model="input"
+                    clearable
+                    class="honor-serche-input">
+                    </el-input>
+                    <el-button type="primary">提交建议</el-button>
+                </div>
+                <div style="margin:10px 40px;">
+                    We don't send spam. Actually, wha are we kiding, we'll spamthe shit out of you'r inbox
+                </div>
             </div>
         </div>
         <div align='center' style="margin-top:15px">
@@ -32,25 +86,21 @@ export default {
     components:{
         welcome,navi,introduction,culture,honor
     },
-    methods : {
-       
-    },
     data(){
-        return{
-           
+        return {
+            input:''
         };
-    },
-    mounted :function(){
-        
     }
 
 }
 </script>
 
 <style>
- .set-div-size{
+ .tail-div-size{
      width: 100%;
      height: 400px;
+     background-color: 	#FAFAD2;
+     
  }
  .honor-index-div1{
      float: left;
@@ -59,5 +109,18 @@ export default {
 .honor-index-div2{
      float: right;
      width: 50%;
+ }
+ .left-tail-div-size{
+     float: left;
+     width: 150px;
+     padding-left:40px;
+ }
+ .left-tail-top-div{
+     text-align: left;
+     margin: 40px 40px;
+     
+ }
+ .honor-serche-input{
+     width: 350px;
  }
 </style>
