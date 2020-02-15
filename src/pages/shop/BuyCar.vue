cbf5e878e8f955ee440411bb32cb44f55d71b254
huzheng <zheng.hu@myt11.com>
2020-02-15 22:37:42 +0800

    购物车页面组件拆分错误，合并购物车单个商品组件到购物车页面组件

--- src/pages/shop/BuyCar.vue
+++ src/pages/shop/BuyCar.vue
@@ -27,9 +27,62 @@
             </el-col>
           </el-row>
       </div>
+      <!--单个商品显示框开始-->
+      <div class="one-shop-div" v-for="i in 5" :key="i">
+        <el-row>
+            <el-col :span="1">
+                <div class="one-shop-checkbox">
+                    <el-checkbox :lable=1></el-checkbox>
+                </div>
+            </el-col>
 
-      <oneShop v-for="i in 5" :key="i"/>
-      
+            <el-col :span="3">
+                <div class="shop-image">
+                    <el-image
+                    style="width: 100px; height: 100px"
+                    :src="url"
+                    :fit="fill">
+                    </el-image>
+                </div>   
+            </el-col>
+
+            <el-col :span="4">
+                <div class="title-link">
+                    <el-link>安踏男针织外套2020春季新款官网旗舰宽松休闲运动卫衣连帽外套男</el-link>
+                </div>
+            </el-col>
+            <el-col :span="4">
+                <div class="shop-size">
+                    颜色分类：黑色 尺码：XXL
+                </div>
+            </el-col>
+            
+            <el-col :span="3">
+                <div class="shop-one-price">
+                    ￥198.00
+                </div>
+            </el-col>
+
+            <el-col :span="4">
+                <div class="shop-number">
+                    <el-input-number size="small" v-model="num" :min="1" :max="10" label="产品数量"></el-input-number>
+                </div>
+            </el-col>
+
+            <el-col :span="3">
+                <div class="shop-sum-price">
+                    <label>￥198.00</label>
+                </div>
+            </el-col>
+
+            <el-col :span="2">
+                <div class="del-shop">
+                    <el-link type="info">删除</el-link>
+                </div>
+            </el-col>
+        </el-row>
+      </div>
+      <!--单个商品显示框结束-->
       <div class="buy-car-tail-div">
         <div>
             <el-row>
@@ -76,12 +129,13 @@
 /**
  * 购物车页面级组件
  */
-import oneShop from '@/components/shop/OneBuyCarShop.vue'
 export default {
-    components: {oneShop},
+    components: {},
     data() {
        return {
-           
+           fill:"fill", //列表图片展示属性
+           url:require("@/assets/phone.jpg"), //列表图片地址
+           num:1, //商品数量  
        };
     },
     methods: {
@@ -140,4 +194,44 @@ export default {
     background-color: #FF4400;
     border-color: #FF4400;
 }
+/**下面是单个商品展示框的样式 */
+    .one-shop-div{
+        margin: 10px auto;
+        border: #ccc 1px solid;
+        width: 988px;
+        height: 130px;
+    }
+
+    .one-shop-checkbox{
+        padding: 5px 10px;
+        margin: 10px 0px;
+    }
+    .shop-image{
+        margin: 10px 0px;
+    }
+    .title-link{
+        padding: 15px 0px;
+        margin: auto 0px;
+    }
+    .shop-size{
+       font-size:12px;
+       padding: 15px 10px;
+       color: #9C9C9C;
+    }
+    .shop-one-price{
+        color: #000;
+        font-weight: 600;
+        padding: 15px;
+    }
+    .shop-number{
+        margin: 15px 0px;
+    }
+    .shop-sum-price{
+        color: #FF4400;
+        margin: 15px 0px;
+        font-weight: 600;
+    }
+    .del-shop{
+        margin: 15px 0px;
+    }
 </style>
