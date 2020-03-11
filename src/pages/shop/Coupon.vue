d89037cfd83f14b9e808a89952c53d90613d82fd
huzheng <zheng.hu@myt11.com>
2020-03-11 10:24:23 +0800

    创建优惠卷页面

---
+++ src/pages/shop/Coupon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+/**
+优惠卷页面
+ */
+export default {
+    components: {},
+    data() {
+       return {};
+    },
+    methods: {
+
+    },
+    created() {
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
